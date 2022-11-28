336eadf6ac24d24a8d903aed04816b90916681ee
이학경 <dlgkrrud00@naver.com>
2022-11-28 20:25:21 +0900

    Update 양꼬치.js

--- lv0/양꼬치.js
+++ lv0/양꼬치.js
@@ -1,3 +1,12 @@
 function solution(n, k) {
   return n * 12000 + k * 2000 - parseInt(n / 10) * 2000
 }
+
+function solution(n, k) {
+  	let answer = 0;
+    if (n >= 10) {
+       k -= Math.floor(n/10);
+    }
+    answer = n*12000+k*2000
+    return answer;
+}
