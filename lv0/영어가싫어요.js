672b0156e22c8cce40695789a5e1ae539dc85f8a
suwoncityBoy <dlgkrrud00@naver.com>
2022-12-08 20:38:51 +0900

    영어가싫어요

---
+++ lv0/영어가싫어요.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+  // "onefourzerosixseven"
+  const number = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  for (let i = 0; i < number.length; i++) {
+    numbers = numbers.split(number[i]).join(i);
+  }
+  return Number(numbers);
+}
