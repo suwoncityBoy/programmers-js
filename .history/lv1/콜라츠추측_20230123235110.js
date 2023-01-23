0ff912f65780a61fe9cbfd4c9a36f66c2d9935fd
suwoncityBoy <dlgkrrud00@naver.com>
2023-01-23 23:54:23 +0900

    20230123

---
+++ .history/lv1/콜라츠추측_20230123235110.js
@@ -0,0 +1,12 @@
+function solution(num) {// 짝수면 2로 나누기 , 홀수면 3을 곱하고 1 더하기 , 이걸 1이 될때까지 반복
+  let cnt = 0
+  while (num !== 1) {
+      if(num%2 === 0) {
+     num = num / 2
+  } else if (num%2 === 1) {
+      num = num * 3 +1
+  }       
+  cnt += 1
+  }
+  cnt >= 500 ? return -1 : return cnt
+}
