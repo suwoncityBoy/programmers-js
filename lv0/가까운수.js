ef3fdcf64179484bdecb295f41e4a08f85f6ebca
suwoncityBoy <dlgkrrud00@naver.com>
2023-02-03 23:41:00 +0900

    20230203

---
+++ lv0/가까운수.js
@@ -0,0 +1,20 @@
+function solution(array, n) {
+  let arr = [];
+  array.sort((a, b) => {
+    return a - b;
+  });
+  for (let i = 0; i < array.length; i++) {
+    arr.push(Math.abs(array[i] - n));
+  }
+
+  return array[arr.indexOf(Math.min(...arr))];
+}
+
+// 다른사람 풀이
+
+const solution = (array, n) =>
+  +array
+    .filter(
+      (a) => Math.abs(a - n) === Math.min(...array.map((a) => Math.abs(a - n)))
+    )
+    .sort((a, b) => a - b)[0];
