9cfc804aedb42b5d69b7c9ebb04693a5bfd37bf5
suwoncityBoy <dlgkrrud00@naver.com>
2023-02-10 23:54:03 +0900

    20230210

---
+++ lv1/k번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+  for (let i = 0; i < array.length; i++) {}
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < commands.length; j++) {
+      array[i];
+    }
+  }
+}
