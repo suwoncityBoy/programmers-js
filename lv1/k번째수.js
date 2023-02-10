9ffc162f7f23d8e0d84a470044185f56c31492e2
suwoncityBoy <dlgkrrud00@naver.com>
2023-02-11 00:26:18 +0900

    20230210

--- lv1/k번째수.js
+++ lv1/k번째수.js
@@ -1,9 +1,16 @@
 function solution(array, commands) {
-  for (let i = 0; i < array.length; i++) {}
+  const solution = [];
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < commands.length; j++) {
-      array[i];
-    }
+  for (let i = 0; i < commands.length; i++) {
+    let x = commands[i][0]; // 2 4 1
+    let y = commands[i][1]; // 5 4 7
+    // slice 0부터 시작하므로 -1 해준다
+    const arr = array.slice(x - 1, y);
+
+    arr.sort((a, b) => a - b);
+
+    let k = arr[commands[i][2] - 1];
+    solution.push(k);
   }
+  return solution;
 }
