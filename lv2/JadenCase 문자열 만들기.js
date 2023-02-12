65bbbb0f3a4575e76353149c88b068ea650a1199
suwoncityBoy <dlgkrrud00@naver.com>
2023-02-12 22:42:53 +0900

    20230212

---
+++ lv2/JadenCase 문자열 만들기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+  let answer = [];
+  let test = s.split(" ");
+  for (let i = 0; i < test.length; i++) {
+    let Upper = test[i].split("");
+    for (let j = 0; j < test[i].length; j++) {
+      if (j == 0) {
+        Upper[j] = Upper[j].toUpperCase();
+      } else {
+        Upper[j] = Upper[j].toLowerCase();
+      }
+    }
+    answer.push(Upper.join(""));
+  }
+  return answer.join(" ");
+}
