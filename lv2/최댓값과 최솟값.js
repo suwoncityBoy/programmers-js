15fc10138ccf87132f45f489caebd0babe4ab371
suwoncityBoy <dlgkrrud00@naver.com>
2023-02-12 21:49:51 +0900

    20230212

--- lv2/최댓값과 최솟값.js
+++ lv2/최댓값과 최솟값.js
@@ -1,3 +1,7 @@
-function () {
-  
+function solution(s) {
+  let array = s.split(" ");
+  let maxValue = Math.max(...array);
+  let minValue = Math.min(...array);
+
+  return minValue + " " + maxValue;
 }
