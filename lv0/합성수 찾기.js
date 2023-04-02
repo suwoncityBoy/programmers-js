799c814e55fd8397ca19721fe6239da90ef7eaee
suwoncityBoy <dlgkrrud00@naver.com>
2023-04-02 22:17:32 +0900

    20230402

--- lv0/합성수 찾기.js
+++ lv0/합성수 찾기.js
@@ -10,3 +10,13 @@ function solution(n) {
   }
   return answer;
 }
+
+function solution(n) {
+  let arr = new Set();
+  for (let i = 1; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) arr.add(i);
+    }
+  }
+  return arr.size;
+}
